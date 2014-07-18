a4473b3f90710b37f6d788883a96a9afa84b8b2a
Matt Jeanes <mattjeanes23@gmail.com>
2014-07-18 20:45:33 +0100

    Added weapon attachments and modified costs
    Attachment costs are placeholder.

--- gamemodes/abyssrp/gamemode/rp_modules/sh_weapons.lua
+++ gamemodes/abyssrp/gamemode/rp_modules/sh_weapons.lua
@@ -1,5 +1,6 @@
 RP.AmmoTypes = {}
 RP.Weapons = {}
+RP.Attachments = {}
 
 function RP:AddAmmoType(t)
 	local n = #RP.AmmoTypes+1
@@ -11,31 +12,37 @@ function RP:AddWeapon(t)
 	RP.Weapons[n]=table.Copy(t)
 end
 
+function RP:AddAttachment(t)
+	local n = #RP.Attachments+1
+	RP.Attachments[n]=table.Copy(t)
+end
+
 -- Ammo types
 
 RP:AddAmmoType({
 	name=".357 SIG",
-	cost=5
+	cost=0.6
 })
 
 RP:AddAmmoType({
 	name=".380 ACP",
-	cost=5
+	cost=0.5
 })
 
+-- Unused apparently
 RP:AddAmmoType({
 	name=".44 Magnum",
-	cost=5
+	cost=0.45
 })
 
 RP:AddAmmoType({
 	name=".45 ACP",
-	cost=5
+	cost=0.5
 })
 
 RP:AddAmmoType({
 	name=".454 Casull",
-	cost=5
+	cost=1
 })
 
 RP:AddAmmoType({
@@ -43,54 +50,56 @@ RP:AddAmmoType({
 	cost=5
 })
 
+-- Unused apparently
 RP:AddAmmoType({
 	name=".50 BMG",
-	cost=5
+	cost=1
 })
 
 RP:AddAmmoType({
 	name="10x25MM",
-	cost=5
+	cost=0.4
 })
 
 RP:AddAmmoType({
 	name="12 Gauge",
-	cost=5
+	cost=0.8
 })
 
 RP:AddAmmoType({
 	name="23x75MMR",
-	cost=5
+	cost=3
 })
 
 RP:AddAmmoType({
 	name="5.45x39MM",
-	cost=5
+	cost=0.2
 })
 
 RP:AddAmmoType({
 	name="5.56x45MM",
-	cost=5
+	cost=0.3
 })
 
 RP:AddAmmoType({
 	name="7.62x39MM",
-	cost=5
+	cost=0.4
 })
 
+-- Unused apparently
 RP:AddAmmoType({
 	name="7.62x51MM",
-	cost=5
+	cost=0.35
 })
 
 RP:AddAmmoType({
 	name="9x18MM",
-	cost=5
+	cost=0.4
 })
 
 RP:AddAmmoType({
 	name="9x19MM",
-	cost=5
+	cost=0.4
 })
 
 -- Weapons
@@ -98,185 +107,319 @@ RP:AddAmmoType({
 RP:AddWeapon({
 	name="AK-47",
 	class="fas2_ak47",
-	cost=5
+	ammotype="7.62x39MM",
+	cost=1700
 })
 
 RP:AddWeapon({
 	name="AK-74",
 	class="fas2_ak74",
-	cost=5
+	ammotype="5.45x39MM",
+	cost=1500
 })
 
 RP:AddWeapon({
 	name="DV2",
 	class="fas2_dv2",
-	cost=5
+	cost=100
 })
 
 RP:AddWeapon({
 	name="FAMAS F1",
 	class="fas2_famas",
-	cost=5
+	ammotype="5.56x45MM",
+	cost=1600
 })
 
 RP:AddWeapon({
 	name="G36C",
 	class="fas2_g36c",
-	cost=5
+	ammotype="5.56x45MM",
+	cost=1500
 })
 
 RP:AddWeapon({
 	name="G3A3",
 	class="fas2_g3",
-	cost=5
+	ammotype="7.62x51MM",
+	cost=1300
 })
 
 RP:AddWeapon({
 	name="Glock-20",
 	class="fas2_glock20",
-	cost=5
+	ammotype="10x25MM",
+	cost=600
 })
 
 RP:AddWeapon({
 	name="IMI Desert Eagle",
 	class="fas2_deagle",
-	cost=5
+	ammotype=".50 AE",
+	cost=1000
 })
 
 RP:AddWeapon({
 	name="IMI Uzi",
 	class="fas2_uzi",
-	cost=5
+	ammotype="9x19MM",
+	cost=900
 })
 
 RP:AddWeapon({
 	name="KS-23",
 	class="fas2_ks23",
-	cost=5
+	ammotype="23x75MMR",
+	cost=1400
 })
 
 RP:AddWeapon({
 	name="M11A1",
 	class="fas2_mac11",
-	cost=5
+	ammotype=".380 ACP",
+	cost=700
 })
 
 RP:AddWeapon({
 	name="M14",
 	class="fas2_m14",
-	cost=5
+	ammotype="7.62x51MM",
+	cost=2000
 })
 
 RP:AddWeapon({
 	name="M1911",
 	class="fas2_m1911",
-	cost=5
+	ammotype=".45 ACP",
+	cost=600
 })
 
 RP:AddWeapon({
 	name="M21",
 	class="fas2_m21",
-	cost=5
+	ammotype="7.62x51MM",
+	cost=2500
 })
 
 RP:AddWeapon({
 	name="M24",
 	class="fas2_m24",
-	cost=5
+	ammotype="7.62x51MM",
+	cost=2300
 })
 
 RP:AddWeapon({
 	name="M3 Super 90",
 	class="fas2_m3s90",
-	cost=5
+	ammotype="12 Gauge",
+	cost=1150
 })
 
 RP:AddWeapon({
 	name="M4A1",
 	class="fas2_m4a1",
-	cost=5
+	ammotype="5.56x45MM",
+	cost=1900
 })
 
 RP:AddWeapon({
 	name="Machete",
 	class="fas2_machete",
-	cost=5
+	cost=250
 })
 
 RP:AddWeapon({
 	name="MP5A5",
 	class="fas2_mp5a5",
-	cost=5
+	ammotype="9x19MM",
+	cost=1000
 })
 
 RP:AddWeapon({
 	name="MP5K",
 	class="fas2_mp5k",
-	cost=5
+	ammotype="9x19MM",
+	cost=1000
 })
 
 RP:AddWeapon({
 	name="MP5SD6",
 	class="fas2_mp5sd6",
-	cost=5
+	ammotype="9x19MM",
+	cost=1000
+})
+
+RP:AddWeapon({
+	name="OTs-33 Pernach"
+	class="fas2_ots33",
+	ammotype="9x18MM",
+	cost=650
 })
 
 RP:AddWeapon({
 	name="P226",
 	class="fas2_p226",
-	cost=5
+	ammotype=".357 SIG",
+	cost=550
 })
 
 RP:AddWeapon({
 	name="PP-19 Bizon",
 	class="fas2_pp19",
-	cost=5
+	ammotype="9x18MM",
+	cost=900
 })
 
 RP:AddWeapon({
 	name="Raging Bull",
 	class="fas2_ragingbull",
-	cost=5
+	ammotype=".454 Casull",
+	cost=1700
 })
 
 RP:AddWeapon({
 	name="Remington 870",
 	class="fas2_rem870",
-	cost=5
+	ammotype="12 Gauge",
+	cost=1500
 })
 
 RP:AddWeapon({
 	name="RPK-47",
 	class="fas2_rpk",
-	cost=5
+	ammotype="7.62x39MM",
+	cost=1600
 })
 
 RP:AddWeapon({
 	name="Sako RK-95",
 	class="fas2_rk95",
-	cost=5
+	ammotype="7.62x39MM",
+	cost=1800
 })
 
 RP:AddWeapon({
 	name="SG 550",
 	class="fas2_sg550",
-	cost=5
+	ammotype="5.56x45MM",
+	cost=1400
 })
 
 RP:AddWeapon({
 	name="SG 552",
 	class="fas2_sg552",
-	cost=5
+	ammotype="5.56x45MM",
+	cost=1400
 })
 
 RP:AddWeapon({
 	name="SKS",
 	class="fas2_sks",
-	cost=5
+	ammotype="7.62x39MM",
+	cost=1800
 })
 
 RP:AddWeapon({
 	name="SR-25",
 	class="fas2_sr25",
-	cost=5
+	ammotype="7.62x51MM",
+	cost=2600
+})
+
+-- Attachments
+
+RP:AddAttachment({
+	name="ACOG 4x",
+	class="fas2_att_acog",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="CompM4",
+	class="fas2_att_compm4",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="ELCAN C79",
+	class="fas2_att_c79",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="EoTech 553",
+	class="fas2_att_eotech",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="Foregrip",
+	class="fas2_att_foregrip",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="Harris Bipod",
+	class="fas2_att_harrisbipod",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="Leupold MK4",
+	class="fas2_att_leupold",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="M21 20RND Mag",
+	class="fas2_att_m2120mag",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="MP5K 30RND Mag",
+	class="fas2_att_mp5k30mag",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="PSO-1",
+	class="fas2_att_pso1",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="SG55X 30RND Mag",
+	class="fas2_att_sg55x30mag",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="SKS 20RND Mag",
+	class="fas2_att_sks20mag",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="SKS 30RND Mag",
+	class="fas2_att_sks30mag",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="Suppressor",
+	class="fas2_att_suppressor",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="Tritium Sights",
+	class="fas2_att_tritiumsights",
+	cost=100
+})
+
+RP:AddAttachment({
+	name="UZI Wooden Stock",
+	class="fas2_att_uziwoodenstock",
+	cost=100
 })
