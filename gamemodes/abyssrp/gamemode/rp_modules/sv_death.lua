a395ec8ac58b1c57064398a44fa3a9b0ccc0a464
Aaron Hands <thekillertoaster@gmail.com>
2015-10-16 19:51:08 +0100

    Revert "Added chatprint for death"
    This reverts commit bb7ad079353a5409bbc56426f5c53f34ecb49c9d.

--- gamemodes/abyssrp/gamemode/rp_modules/sv_death.lua
+++
@@ -1,5 +0,0 @@
-hook.Add( "PlayerDeath", "Player-Death", function(victim, weapon, killer)
-	if victim:IsPlayer() and killer:IsPlayer() and killer != victim then
-		victim:ChatPrint("You were killed by player: " .. killer:Nick() .. " Using: " .. weapon:Nick())
-	end
-end)
